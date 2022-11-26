2c2183839567ccd08aa43be57705a392e1ee904d
vande <rakelmn@gmail.com>
2022-11-26 13:06:15 -0500

    Initial Uodates to viz

--- static/js/charts.js
+++ static/js/charts.js
@@ -4,8 +4,8 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
-    var sampleNames = data.names;
+  d3.json("Womens_Clothing_E-Commerce_Reviews.json").then((data) => {
+    var sampleNames = data.ClothingID;
 
     sampleNames.forEach((sample) => {
       selector
@@ -33,7 +33,7 @@ function optionChanged(newSample) {
 
 // Product Info Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("samples.Womens_Clothing_E-Commerce_Reviews.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -58,7 +58,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the out.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("Womens_Clothing_E-Commerce_Reviews.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples; 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
